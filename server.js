b3d9de901ac7f0942b59ccc457bb9820da92113d
seanvsville <seanvanho@gmail.com>
2016-05-25 20:28:56 -0700

    added all category crud routes to server.js

--- server.js
+++ server.js
@@ -37,6 +37,42 @@ app.get('/', function homepage (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+// get all categories
+app.get('/api/categories', function (req, res) {
+  // send all categories as JSON response
+  db.Category.find(function(err, categories) {
+    if (err) { return console.log('index error: ', err); }
+    res.json(categories);
+  });
+});
+
+// get one category
+app.get('/api/categories/:id', function (req, res) {
+  db.Categories.findOne({ _id: req.params._id }, function(err, data) {
+    res.json(data);
+  });
+});
+
+// create new category
+app.post('/api/categories', function (req, res) {
+  // create new category with form data ('req.body')
+  console.log('category created: ', req.body);
+  var newCategory = new db.Category(req.body);
+  newCategory.save(function handleDBCategorySaved(err, savedCategory) {
+    res.json(savedCategory);
+  });
+});
+
+// delete category
+app.delete('/api/categories/:id', function (req, res) {
+  // get category id from URL params ('req.params')
+  console.log('category to be deleted', req.params);
+  var categoryId = req.params.id;
+  // find the index of the book we want to remove
+  db.Category.findOneAndRemove({ _id: categoryId }, function (err, deletedCategory) {
+    res.json(deletedCategory);
+  });
+});
 
 /*
  * JSON API Endpoints
