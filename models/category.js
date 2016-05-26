a58303e892273cc3e7ba2edac7fe4eed054952eb
seanvsville <seanvanho@gmail.com>
2016-05-25 21:10:32 -0700

    added product as reference under category model

--- models/category.js
+++ models/category.js
@@ -3,7 +3,11 @@ var Schema   = mongoose.Schema;
 
 var CategorySchema = new Schema({
   name: String,
-  description: String
+  description: String,
+  product: {
+    type: Schema.Types.ObjectId,
+    ref: 'Author'
+  },
 });
 
 var Category = mongoose.model('Category', CategorySchema);
--- models/index.js
+++ models/index.js
@@ -1,4 +1,5 @@
-var mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/rubber-ducky-store_test");
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/rubber-ducky-store_test');
 
-module.exports.Category = require("./category.js");
+module.exports.Category = require('./category.js');
+module.exports.Product = require('./product.js');
